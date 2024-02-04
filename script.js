a319da3366915aab7cf16efaa7e6e452c52104c9
Chirag Agarwal <72219269+chirag123771@users.noreply.github.com>
2024-02-05 02:32:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 //complete this code
-class Rectangle {}
+class Rectangle { constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
 
-class Square extends Animal {}
+  getArea() {
+    return this.width * this.height;
+  }}
+
+class Square extends Animal {  constructor(side) {
+    super(side, side);
+  }
+
+  getPerimeter() {
+    return 4 * this.width; // Perimeter of a square: 4 * side
+  }}
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
